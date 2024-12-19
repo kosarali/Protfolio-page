6ff6636e1b8062a027f73b3e658b65b0a9aa123e
kosarali <145735346+kosarali@users.noreply.github.com>
2024-12-19 18:19:51 +0530

    Add files via upload

---
+++ kosarali/header.php
@@ -0,0 +1,41 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio Demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+ <!-- Navbar -->
+ <nav class="navbar navbar-expand-sm" style="background-color: orange;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/home.php">RTW</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample03">
+          <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Solutions</a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Web Application Development</a></li>
+                <li><a class="dropdown-item" href="#">Mobile Application Development</a></li>
+                <li><a class="dropdown-item" href="#">Cloud Services</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="http://localhost/kosarali/portfolio.php">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Case Study</a>
+            </li>
+          </ul>
+          <form role="search">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+          </form>
+        </div>
+      </div>
+    </nav>
---
+++ kosarali/mobile_more.php
@@ -0,0 +1,18 @@
+<?php include('header.php'); ?>
+
+<div class="container-fluid my-5">
+      <div class="jumbotron">
+        <div class="container-fluid text-light text-center py-5" style="background: url('assets/img/2.jpg') no-repeat center center; background-size: cover;">
+          <h1 class="display-4 fw-bold">Mobile Application Development Projects</h1>
+          <p class="text-white">A showcase of my best projects and achievements</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="text-center mt-5">
+        <a href="sf_more.php" class="btn btn-outline-secondary">View Software Development Projects</a>
+        <a href="web_more.php" class="btn btn-outline-secondary">View Web Development Projects</a>
+      </div>
+    </div>
+
+<?php include('footer.php'); ?>
